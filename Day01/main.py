5870721865a6ce526d51ce086d3ab5a4dcccb558
Chris Harris <charris@microsoft.com>
2021-12-02 22:51:46 -0800

    first answer

--- Day01/main.py
+++ Day01/main.py
@@ -1,8 +1,18 @@
 from rich import print, inspect
 
-print("Hello World!")
-
 with open('data.txt') as f:
     lines = f.readlines()
 
-inspect(lines)
+# inspect(lines)
+
+previous = 0
+largerCount = 0
+
+for line in lines:
+    line = int(line)
+    if line > previous:
+        largerCount +=1
+    previous = line
+
+print(f"largerCount = {largerCount}")
+print(f"Length = {len(lines)}")
